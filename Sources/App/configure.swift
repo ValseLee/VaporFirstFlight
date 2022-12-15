1d5afc3ace1b704cb77a14ff57f03d848f5ffc63
valseLee <sollleky72@gmail.com>
2022-12-15 23:16:06 +0900

    221215: 라우팅 경로 지정, db configure 추가

--- Sources/App/configure.swift
+++ Sources/App/configure.swift
@@ -1,19 +1,15 @@
 import Fluent
 import FluentPostgresDriver
+import FluentMySQLDriver
 import Vapor
 
 // configures your application
 public func configure(_ app: Application) throws {
     // uncomment to serve files from /Public folder
     // app.middleware.use(FileMiddleware(publicDirectory: app.directory.publicDirectory))
-
-    app.databases.use(.postgres(
-        hostname: Environment.get("DATABASE_HOST") ?? "localhost",
-        port: Environment.get("DATABASE_PORT").flatMap(Int.init(_:)) ?? PostgresConfiguration.ianaPortNumber,
-        username: Environment.get("DATABASE_USERNAME") ?? "vapor_username",
-        password: Environment.get("DATABASE_PASSWORD") ?? "vapor_password",
-        database: Environment.get("DATABASE_NAME") ?? "vapor_database"
-    ), as: .psql)
+	
+	/// test DBTable을 db로 사용, 당연히 테스트라서 이름과 패스워드는 valse, 1234로 설정하고 따로 보안조치 하지 않음
+	app.databases.use(.mysql(hostname: "localhost", username: "valse", password: "1234", database: "test"), as: .mysql)
 
     app.migrations.add(CreateTodo())
 
