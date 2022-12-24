b849b577b15b8d9109e04613668a3eee6d5e991e
valseLee <sollleky72@gmail.com>
2022-12-24 21:01:13 +0900

    [Init] Vapor의 정보를 호출하며 뷰에서 그리는 연습 용도 프로젝트 init

---
+++ VaporPresenter/VaporPresenter/VaporPresenterApp.swift
@@ -0,0 +1,17 @@
+//
+//  VaporPresenterApp.swift
+//  VaporPresenter
+//
+//  Created by 이승준 on 2022/12/24.
+//
+
+import SwiftUI
+
+@main
+struct VaporPresenterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
